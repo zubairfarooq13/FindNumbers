ae26220d7d8b182e9c61374eea95ec4eeaaab92f
Zubair Farooq <43909324+zubairfarooq13@users.noreply.github.com>
2022-08-17 18:54:22 +0500

    Add project files.

---
+++ FindNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Input: nums = [12, 345, 2, 6, 7896]
+Output: 2
+Explanation:
+12 contains 2 digits(even number of digits).
+345 contains 3 digits(odd number of digits).
+2 contains 1 digit(odd number of digits).
+6 contains 1 digit(odd number of digits).
+7896 contains 4 digits(even number of digits).
+Therefore only 12 and 7896 contain an even number of digits.*/
+
+
+//initialize nums array
+int[] nums = { 12, 345, 2, 6, 7896 };
+List<int> EvenNumCount = new List<int>();
+
+//Convert nums array to String
+string[] numsToString = Array.ConvertAll(nums, str => str.ToString());
+
+foreach (var num in numsToString)
+{
+	if(Convert.ToUInt32(num.Length) % 2 == 0)
+	{
+		EvenNumCount.Add(Convert.ToInt32(num));
+	}
+}
+
+Console.WriteLine(EvenNumCount.Count());
+
